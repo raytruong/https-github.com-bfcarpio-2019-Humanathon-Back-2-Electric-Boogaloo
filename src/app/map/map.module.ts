5b4779b489152ddae75cd0c5f5dc55566fa44ef5
Raymond Truong <raylintruong@gmail.com>
2019-07-26 14:23:42 -0400

    added map page

--- src/app/map/map.module.ts
+++ src/app/map/map.module.ts
@@ -1,10 +1,9 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
-
-import { MapPage } from './map.page';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { IonicModule } from "@ionic/angular";
+import { RouterModule } from "@angular/router";
+import { MapPage } from "./map.page";
 
 @NgModule({
   imports: [
@@ -13,11 +12,11 @@ import { MapPage } from './map.page';
     IonicModule,
     RouterModule.forChild([
       {
-        path: '',
+        path: "",
         component: MapPage
       }
     ])
   ],
   declarations: [MapPage]
 })
-export class MapPageModule { }
+export class MapPageModule {}
--- src/app/map/map.page.ts
+++ src/app/map/map.page.ts
@@ -1,36 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+import { Map, latLng, tileLayer, Layer, marker } from "leaflet";
 
 @Component({
-  selector: 'app-map',
-  templateUrl: 'map.page.html',
-  styleUrls: ['map.page.scss']
+  selector: "app-map",
+  templateUrl: "map.page.html",
+  styleUrls: ["map.page.scss"]
 })
 export class MapPage implements OnInit {
   private selectedItem: any;
   private icons = [
-    'flask',
-    'wifi',
-    'beer',
-    'football',
-    'basketball',
-    'paper-plane',
-    'american-football',
-    'boat',
-    'bluetooth',
-    'build'
+    "flask",
+    "wifi",
+    "beer",
+    "football",
+    "basketball",
+    "paper-plane",
+    "american-football",
+    "boat",
+    "bluetooth",
+    "build"
   ];
   public items: Array<{ title: string; note: string; icon: string }> = [];
   constructor() {
     for (let i = 1; i < 11; i++) {
       this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
+        title: "Item " + i,
+        note: "This is item #" + i,
         icon: this.icons[Math.floor(Math.random() * this.icons.length)]
       });
     }
   }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  //Lifecycle hooks
+  map: Map;
+  lat: number = 5;
+  lng: number = 5;
+
+  ionViewDidEnter() {
+    this.loadMap();
+  }
+
+  loadMap() {
+    this.map = new Map("map").setView([this.lat, this.lng], 8);
+
+    tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18
+    }).addTo(this.map);
   }
   // add back when alpha.4 is out
   // navigate(item) {
